d05234e5c42a0f9dda780f79c3f5b4b9d5894fb0
kachergis <george.kachergis@gmail.com>
2022-05-05 09:22:43 -0700

    Kigiriama done

---
+++ Kigiriama.R
@@ -0,0 +1,33 @@
+library(wordbankr)
+library(tidyverse)
+source("update_unilemmas.R")
+
+
+language = "Kigiriama"
+outdir = "final_instruments/"
+
+#old_items <- get_item_data(language = language) %>%
+#  filter(type=="word")
+
+new_items <- update_unilemmas(language, show_conflicts=T)
+# "loaded 773 Kigiriama items"
+# "428 missing uni_lemmas in original ( 55 % missing)"
+# "303 uni_lemmas added/modified"
+# "Adding uncontested new_uni_lemmas..."
+# "144 uni_lemmas still missing ( 19 % missing)"
+# "Review 14 suggested new_uni_lemmas
+
+new_items[which(new_items$WS=="item_1"),]$uni_lemma = "baa baa" # sound, not animal
+new_items[which(new_items$WS=="item_4"),]$uni_lemma = "quack quack" # sound, not animal
+new_items[which(new_items$WS=="item_5"),]$uni_lemma = "hee haw" # sound, not animal
+new_items[which(new_items$WS=="item_6"),]$uni_lemma = "woof woof" # sound, not animal
+new_items[which(new_items$WS=="item_12"),]$uni_lemma = "baa baa" # goat -- close enough to sheep?
+
+# ToDo: check empty uni-lemmas ("")
+
+# save updated instrument(s)
+new_ws <- update_instrument(language, "WS", new_items %>% rename(itemID = WS))
+# "527 new uni-lemmas defined for Kigiriama"
+
+new_wg <- update_instrument(language, "WG", new_items %>% rename(itemID = WG))
+# "253 new uni-lemmas defined for Kigiriama"
